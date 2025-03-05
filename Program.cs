ada5a3057706d84bd11b21fdf118c2813580c8a6
Ali TEKÇE <alitekce1903@gmail.com>
2025-03-05 21:09:17 +0300

    PRINTING_THE_ARRAYS_IN_REVERSE
    PRINTING THE ARRAYS IN REVERSE FOR C#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme_odası_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] dizi = { 1, 2, 3, 4, 5 };
+                int tut;
+
+                // Orjinal diziyi yazdırma
+                Console.WriteLine("Orjinal hali");
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("{0} ", dizi[i]);
+                }
+
+                // Diziyi tersine çevirme
+                Console.WriteLine("\nTers hali");
+                for (int i = 0; i < 3; i++)
+                {
+                    tut = dizi[i];
+                    dizi[i] = dizi[5 - i - 1];
+                    dizi[5 - i - 1] = tut;
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("{0} ", dizi[i]);
+                }
+            
+        }
+
+    }
+}
